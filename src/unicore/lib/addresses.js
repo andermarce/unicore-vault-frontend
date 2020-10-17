50003ba7c0d1b80d72fd4d8afe6902beacf08cf6
QuantSoldier <quantsoldier@gmail.com>
2020-10-17 08:56:06 -0700

    Dry run

--- src/unicore/lib/addresses.js
+++ src/unicore/lib/addresses.js
@@ -9,10 +9,10 @@ const Addresses = {
     vaults: []
   },
   4: {
-    uniCore: '0x311c58Bf5f18A2657aB0ad981a3a4521a15F02b3',
-    uniCoreLp: '0x1629FcC14Dd62D1Eff28927e7fE5CEEB80E73123',
-    uniCoreWrapped: '0xa45919a0e0Cdd4fA79dcD41d9bEA1CcDC3181d13',
-    uniCoreVault: '0x8b12446d29fF18e541B1ceE09508D57912f4dd69',
+    uniCore: '0xD97d824d6946A9B665E9E653c894Ee8B41a15cAB',
+    uniCoreLp: '0x23B1c517ae6027FB989DC53075Fc167e0a97Ca71',
+    uniCoreWrapped: '0x0d1AC513689Ed80a6CF1E7D6c259F23c3f50c6f0',
+    uniCoreVault: '0x5D7C763eea16b45D0621E995856480bC432a847B',
     uniswapFactory: '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95',
     uniswapFactoryV2: '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
     weth: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
